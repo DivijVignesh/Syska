9733ae2248a2c59fbd9bed2669e9f4389ddf9f25
DivijVignesh <72255233+DivijVignesh@users.noreply.github.com>
2020-10-10 22:27:46 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client , message) =>{
     console.log('SyskaBot is online!'); // logginng on the console will tell us whether the bot is online or not
-    client.user.setActivity('=cmds', { type: "LISTENING" }); // Setting of activity
+    client.user.setActivity('=cmds !', { type: "LISTENING" }); // Setting of activity
     // client.user.setUsername('Syska Bot'); // sets the bots name
     // client.user.setStatus("online"); // sets the bots status
 }
