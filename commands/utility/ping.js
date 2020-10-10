b9db4ddf780cb026779f155b99d5b8165c886fa1
DivijVignesh <72255233+DivijVignesh@users.noreply.github.com>
2020-10-10 23:16:19 +0530

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,10 +4,12 @@ module.exports={
     description: 'This command gives the ping ',
     run: async(client , message , args)=>{
         if (message.content === '=ping') {  
+            const ping = await Date.now()-message.createdTimestamp;
+            const api = await Math.round(client.ws.ping);
             const pingembed = new MessageEmbed()
             .setColor('#e2b31a')
             .setTitle('Bot Ping')
-            .setDescription(`Latency: ${Math.round(client.ws.ping)}ms`)
+            .setDescription(`Your ping is ${ping} ms\nAPI Latency: ${api}ms`)
             .setFooter('Syska Bot')
             .setTimestamp();
             message.channel.send(pingembed);
