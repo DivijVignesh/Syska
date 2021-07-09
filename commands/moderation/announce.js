e678d6575fbe6cf68df72dcd2b07e851927c1799
Divij Vignesh <divijvignesh12@gmail.com>
2021-07-09 17:19:15 +0530

    Updated announce command

--- commands/moderation/announce.js
+++ commands/moderation/announce.js
@@ -1,15 +1,32 @@
+const { MessageEmbed } = require("discord.js")
 module.exports = {
     name: 'announce',
     description: "posts a message behalf of a administrator",
     category: 'moderation',
 
     run: async(client , message, args)=> {
+        if(!message.member.hasPermission("ADMINISTRATOR")){
+            message.channel.send("You do not have the permission to use this command");
+            return;
+        }
         var msg = '';
         let a = args.length;
         for (var i = 0; i < a; i++) {
             msg = msg+ args[i] + ' ';
         }
-        message.channel.send(msg);
+        try {
+                    const exampleEmbed = new MessageEmbed()
+                        .setColor('#0099ff')
+                        .setAuthor(msg)
+                        
+                       message.channel.send(exampleEmbed);
+                    //client.users.cache.get(user.id).send(`Message sent by ${send} \n ` + dmmessage);
+                    console.log('Added announcement');
+                } catch (err) {
+                    message.reply('Error sending a Dm message');
+                    console.log(err);
+
+                }
         message.delete();
     }
 }
